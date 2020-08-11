346154cc76eac3b43cbbba3ecfebc27d26675a14
brianadit24 <brian.adityaherman@gmail.com>
2020-08-11 23:20:58 +0700

    Add History Calculating

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -4,23 +4,23 @@ const calculator = {
    firstNumber: null,
    waitingForSecondNumber: false
 };
- 
+
 function updateDisplay() {
    document.getElementById("displayNumber").innerText = calculator.displayNumber;
 }
- 
+
 function clearCalculator() {
    calculator.displayNumber = '0';
    calculator.operator = null;
    calculator.firstNumber = null;
    calculator.waitingForSecondNumber = false;
 }
- 
+
 function inputDigit(digit) {
    if (calculator.displayNumber === '0') {
-       calculator.displayNumber = digit;
+      calculator.displayNumber = digit;
    } else {
-       calculator.displayNumber += digit;
+      calculator.displayNumber += digit;
    }
 }
 
@@ -44,55 +44,64 @@ function handleOperator(operator) {
 }
 
 function performCalculation() {
-   if(calculator.firstNumber == null || calculator.operator == null) {
+   if (calculator.firstNumber == null || calculator.operator == null) {
       alert('Anda belum menetapkan operator');
       return;
    }
 
    let result = 0;
-   if(calculator.operator === '+') {
+   if (calculator.operator === '+') {
       result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
    } else {
       result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
    }
 
+   // objek yang akan dikirimkan sebagai argumen fungsi putHistory()
+   const history = {
+      firstNumber: calculator.firstNumber,
+      secondNumber: calculator.displayNumber,
+      operator: calculator.operator,
+      result: result
+   }
+   putHistory(history);
    calculator.displayNumber = result;
+   renderHistory();
 }
 
- 
+
 const buttons = document.querySelectorAll(".button");
 for (let button of buttons) {
-   button.addEventListener('click', function(event) {
-    
-    
-       // mendapatkan objek elemen yang diklik
-       const target = event.target;
+   button.addEventListener('click', function (event) {
 
-       if(target.classList.contains('clear')) {
-           clearCalculator();
-           updateDisplay();
-           return;
-       }
 
-       if(target.classList.contains('negative')) {
-          inverseNumber();
-          updateDisplay();
-          return;
-       }
+      // mendapatkan objek elemen yang diklik
+      const target = event.target;
 
-       if(target.classList.contains('equals')) {
+      if (target.classList.contains('clear')) {
+         clearCalculator();
+         updateDisplay();
+         return;
+      }
+
+      if (target.classList.contains('negative')) {
+         inverseNumber();
+         updateDisplay();
+         return;
+      }
+
+      if (target.classList.contains('equals')) {
          performCalculation();
          updateDisplay();
          return;
       }
 
-      if(target.classList.contains('operator')) {
+      if (target.classList.contains('operator')) {
          handleOperator(target.innerText);
          updateDisplay();
          return;
       }
-      
-       inputDigit(target.innerText);
-       updateDisplay()
+
+      inputDigit(target.innerText);
+      updateDisplay()
    });
 }
